1fdc631809ba9af3eee53e8d8437895e42c6801c
Pedro Felipe Zambrano García <pfzambrano@gmail.com>
2026-02-12 11:28:20 -0600

    Actualización 11/02/2026

--- senosiain/Ajuste Prog Vacaciones/consultas.sql
+++ senosiain/Ajuste Prog Vacaciones/consultas.sql
@@ -1,22 +1,3 @@
-Select *
-from dbo.transacciones_ns a
-Where  exists ( Select Top 1 1
-                From   progVacMgrTbl
-                Where  compania = a.compania
-                And    trabajador = a.trabajador
-                And    ciclolaboral = a.referencia)
-And referencia != '';
-
-
-Select distinct a.compania, a.trabajador, a.ciclo_laboral
-From   dbo.programacion_vacaciones a
-Where  exists ( Select Top 1 1
-                From   progVacMgrTbl
-                Where  compania     = a.compania
-                And    trabajador   = a.trabajador
-                And    ciclolaboral = a.ciclo_laboral);
-
-
 declare
     @w_comilla Char(1) = Char(39);
 
@@ -24,13 +5,13 @@ Select Concat('Insert into dbo.programacion_vacaciones Select ',
        @w_comilla, compania,  @w_comilla, ', ', @w_comilla, trabajador, @w_comilla, ', ',
        @w_comilla, ciclo_laboral, @w_comilla, ', ',      tipo_ciclo, ', ',
        sec_prog_vac, ', ', secuencia_ciclo, ', ', tiempo_prog_vac, ', ',
-       @w_comilla, Cast(fecha_ini_per_vac As Varchar), @w_comilla, ', ',
-       @w_comilla, Cast(fecha_fin_per_vac As Varchar), @w_comilla, ', ',
-       @w_comilla, Cast(fecha_pago_vac    As Varchar), @w_comilla, ', ',
+       'Cast(', @w_comilla, Convert(Char(10), fecha_ini_per_vac, 120), @w_comilla, ' As Date) ', ', ',
+       'Cast(', @w_comilla, Convert(Char(10), fecha_fin_per_vac, 120), @w_comilla, ' As Date) ', ', ',
+       'Cast(', @w_comilla, Convert(Char(10), fecha_pago_vac,    120), @w_comilla, ' As Date) ', ', ',
        situacion_programa, ', ', tipo_vacaciones, ', ',
        Case When manejo_dias_vac Is Null
-            Then Null
-       Else manejo_dias_vac
+            Then 1
+            Else manejo_dias_vac
        End, 
        ', ', tipo_mov_vac)
 From   dbo.programacion_vacaciones a
